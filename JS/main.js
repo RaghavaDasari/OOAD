6d11ace914586b2516100d5149fcf50ed26ce56f
draghavalee <40686426+draghavalee@users.noreply.github.com>
2019-02-25 21:17:29 +0530

    Add files via upload

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,49 @@
+$(document).ready(function(){
+  $(window).scrollTop(0);
+  $('body').css('overflow-y', 'hidden');
+    setTimeout(function(){
+      $('#menu a').addClass('bounceInLeft');
+      $('#menu a').css('display','block');
+      $('video').animate({
+        opacity: 0
+      },100,function(){
+        $('#sec .ls').animate({
+          opacity: 1,
+          'z-index': 100
+      },100);
+        $('#sec .ls').addClass('bounceInDown');
+        $('#sec p').css('display','block');
+        $('#sec #text').addClass('lightSpeedIn');
+        $('#sec #text1').addClass('rollIn');
+        $('#sec #text2').addClass('bounceInRight');
+        $('#sec #indic').css('display','block');
+        $('#sec #indic').addClass('bounce');
+        $('body').css('overflow-y','visible');
+      });
+  },7300);
+  $('#sec #indic').click(function(){
+    $(window).scrollTop($('#sec1').offset().top);
+  });
 
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsive:{
+        0:{
+            nav:true,
+            items:1
+        },
+        600:{
+            nav:true,
+            items:3
+        },
+        1000:{
+            nav:true,
+            items:5
+        }
+    },
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true
+  });
+});
